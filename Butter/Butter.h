a571184c401a27a3c717003e35a1ad10e9b584d5
Stephen Horne <fatste@gmail.com>
2017-11-27 20:34:24 +0000

    Initial Commit

---
+++ Butter/Butter.h
@@ -0,0 +1,19 @@
+//
+//  Butter.h
+//  Butter
+//
+//  Created by Stephen Horne on 27/11/17.
+//  Copyright © 2017 Rockfax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Butter.
+FOUNDATION_EXPORT double ButterVersionNumber;
+
+//! Project version string for Butter.
+FOUNDATION_EXPORT const unsigned char ButterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Butter/PublicHeader.h>
+
+
